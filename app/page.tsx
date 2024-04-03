2d1e26c0d89159246461690ccd3143ba57ea20f0
sithomo022 <sithomo022@student.wethinkcode.co.za>
2024-04-03 21:39:50 +0200

    button centered

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,37 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			{/* Landing Section */}
-			<div className="max-w-6xl justify-items-center">
-				<h1 className="text-slate-700 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white">
+			<div className="max-w-6xl">
+				<h1
+					className="text-slate-700 font-extrabold text-4xl sm:text-5xl
+                    lg:text-6xl tracking-tight text-center dark:text-white"
+				>
 					Streamline Patient Care and Practice Efficiency with{" "}
 					<span className="text-primary-600">Ubuntu Health</span>
 				</h1>
-				<p className="text-base text-body-color">
+				<p className="text-base text-body-color my-6">
 					Experience the simplicity of managing patient records, scheduling
 					appointments, and enhancing communication, all in one secure platform.
 					Our EHR web app is designed to streamline your practice workflow,
 					ensuring compliance and providing peace of mind.
 				</p>
-				<a className="text-primary-50 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Join The Waiting List
-				</a>
+				<div className="flex justify-center items-center">
+					<a
+						className="text-primary-50 bg-primary-600 hover:bg-primary-700
+                    focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full
+                    text-lg px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
+                    dark:focus:ring-blue-800"
+					>
+						Join The Waiting List
+					</a>
+				</div>
 			</div>
 
 			{/* Mission Statement */}
 			<div className="flex flex-wrap -mx-4">
 				<div className="w-full px-4">
 					<div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-						<h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
+						<h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4 py-6">
 							Our Mission
 						</h2>
 						<p className="text-base text-body-color">
@@ -39,7 +49,7 @@ export default function Home() {
 			<div className="flex flex-wrap -mx-4">
 				<div className="w-full px-4">
 					<div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-						<h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4">
+						<h2 className="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark">
 							What We Offer
 						</h2>
 					</div>
