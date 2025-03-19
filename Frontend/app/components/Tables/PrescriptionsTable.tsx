64bd55050b44592f448337abff6facbf7cbaf511
Siphesihle Thomo <seansthomo@gmail.com>
2025-03-19 10:11:57 +0200

    FEATURE: implement prescriptionTable

--- Frontend/app/components/Tables/PrescriptionsTable.tsx
+++ Frontend/app/components/Tables/PrescriptionsTable.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import { div } from "framer-motion/client";
 
 interface Prescription {
 	id: number;
@@ -19,9 +18,38 @@ const PrescriptionsTableCard: React.FC<PrescriptionsCardProps> = ({
 	prescriptions = [],
 }) => {
 	return (
-		<div className="relative overflow-x-auto">
+		<div className="relative overflow-x-auto sm:rounded-tr-lg sm:rounded-tl-lg">
 			<table className="w-full text-sm text-left">
-				<thead className="text-sx uppercase"></thead>
+				<thead className="text-sx uppercase">
+					<tr>
+						<th scope="col" className="px-6 py-3">
+							Patient ID
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Medication
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Dosage
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Instructions
+						</th>
+						<th scope="col" className="px-6 py-3">
+							Issue Date
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{prescriptions.map((prescription) => (
+						<tr key={prescription.id} className="border-b">
+							<td className="px py-4">{prescription.patientId}</td>
+							<td className="px py-4">{prescription.medicationName}</td>
+							<td className="px py-4">{prescription.dosage}</td>
+							<td className="px py-4">{prescription.instructions}</td>
+							<td className="px py-4">{prescription.issueDate}</td>
+						</tr>
+					))}
+				</tbody>
 			</table>
 		</div>
 	);
