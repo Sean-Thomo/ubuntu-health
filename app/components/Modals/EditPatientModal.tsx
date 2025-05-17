7563c4cb1cfbfe27bfc1c836f1b06d47c65a2a8f
Siphesihle Thomo <seansthomo@gmail.com>
2025-05-17 17:54:28 +0200

    FIX: fix posible null values in edit patient modal

--- app/components/Modals/EditPatientModal.tsx
+++ app/components/Modals/EditPatientModal.tsx
@@ -210,7 +210,7 @@ const EditPatientModal = ({
 									type="text"
 									name="street"
 									id="street"
-									value={formData.street}
+									value={formData.street ?? ""}
 									onChange={handleChange}
 									className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none
                 focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
@@ -324,7 +324,7 @@ const EditPatientModal = ({
 							</div>
 
 							{/* Relationship */}
-							{/* <div>
+							<div>
 								<label
 									htmlFor="emergencyContactRelationship"
 									className="block text-sm font-medium text-gray-700 mb-1"
@@ -335,13 +335,13 @@ const EditPatientModal = ({
 									type="text"
 									name="emergencyContactRelationship"
 									id="emergencyContactRelationship"
-									value={formData.emergencyContactRelationship}
+									value={formData.emergencyContactRelationship ?? ""}
 									onChange={handleChange}
 									placeholder="Mother"
 									className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none
                 focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
 								/>
-							</div> */}
+							</div>
 
 							{/* Contact Number */}
 							<div>
